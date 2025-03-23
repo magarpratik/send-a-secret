5f0717fa23f75aaca21e2096754e43f2d77c266b
Pratik Magar <67797986+magarpratik@users.noreply.github.com>
2025-03-23 18:22:21 +0000

    test workflow

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit - TESTING TESTING TESTING TESTING TESTING TESTING</h1>
+<h1>Welcome to SvelteKit - TESTING TESTING TESTING TESTING TESTING</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
