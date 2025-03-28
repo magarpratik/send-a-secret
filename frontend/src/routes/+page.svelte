ae373e5a630d34310a8e7931ef0a731dca8fb78d
Pratik Magar <magarpratik@live.co.uk>
2025-03-28 22:04:19 +0000

    center elements

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 </script>
 
-<div class="flex flex-col sm:flex-row gap-2 sm:gap-4 items-center justify-center h-screen">
-  <input type="text" placeholder="Enter secret..." class="input" />
-  <button class="btn btn-soft btn-primary">Generate link</button>
+<div class="flex h-screen flex-col items-center justify-center gap-2 sm:flex-row sm:gap-4">
+	<input type="text" placeholder="Enter secret..." class="input" />
+	<button class="btn btn-soft btn-primary">Generate link</button>
 </div>
