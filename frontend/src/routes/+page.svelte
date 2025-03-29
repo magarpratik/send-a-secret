4bdc08c00acb42bb44981b28b77f9c801b096211
Pratik Magar <magarpratik@live.co.uk>
2025-03-29 08:35:34 +0000

    set title

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,6 +6,11 @@
 	}
 </script>
 
+<div class="fixed top-0 z-10 w-full py-20 text-center">
+	<h1 class="text-4xl font-bold text-gray-400">Send a secret!</h1>
+	<p class="text-gray-600">Send secrets securely using a one-time-use link</p>
+</div>
+
 <div class="flex h-screen flex-col items-center justify-center gap-2 sm:flex-row sm:gap-4">
 	<textarea class="textarea" placeholder="Enter secret..."></textarea>
 	<button class="btn btn-soft btn-primary" on:click={showComingSoon}>Generate link</button>
