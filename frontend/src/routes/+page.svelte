c8dcba6f1529db10dc4ea555d55eaf6acacfe6d5
Pratik Magar <magarpratik@live.co.uk>
2025-03-29 08:15:43 +0000

    use text area

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -7,6 +7,6 @@
 </script>
 
 <div class="flex h-screen flex-col items-center justify-center gap-2 sm:flex-row sm:gap-4">
-	<input type="text" placeholder="Enter secret..." class="input" />
+	<textarea class="textarea" placeholder="Enter secret..."></textarea>
 	<button class="btn btn-soft btn-primary" on:click={showComingSoon}>Generate link</button>
 </div>
