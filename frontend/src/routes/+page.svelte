cb257f93c9a18c46cdbfe21e406875de1440aa6c
Pratik Magar <magarpratik@live.co.uk>
2025-03-29 07:49:39 +0000

    trigger deployment

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,5 +4,5 @@
 
 <div class="flex h-screen flex-col items-center justify-center gap-2 sm:flex-row sm:gap-4">
 	<input type="text" placeholder="Enter secret..." class="input" />
-	<button class="btn btn-soft btn-primary">Generate link</button>
+	<button class="btn btn-soft btn-primary">Generate link!</button>
 </div>
