86272d01f934ed33ecf51cfdadf48bfc369861c3
Pratik Magar <magarpratik@live.co.uk>
2025-03-29 09:01:46 +0000

    add github link

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -11,6 +11,16 @@
 	<p class="text-gray-600">Send secrets securely using a one-time-use link</p>
 </div>
 
+<div class="fixed top-0 right-0 z-20 p-5">
+	<a
+		href="https://github.com/magarpratik/send-a-secret"
+		target="_blank"
+		class="font-semibold text-blue-600 hover:text-blue-800"
+	>
+		GitHub
+	</a>
+</div>
+
 <div class="flex h-screen flex-col items-center justify-center gap-2 sm:flex-row sm:gap-4">
 	<textarea class="textarea" placeholder="Enter secret..."></textarea>
 	<button class="btn btn-soft btn-primary" on:click={showComingSoon}>Generate link</button>
