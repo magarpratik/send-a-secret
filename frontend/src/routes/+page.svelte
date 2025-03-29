02cda8c8db412da6add3f7ff4cccf09170774c75
Pratik Magar <magarpratik@live.co.uk>
2025-03-29 08:04:19 +0000

    add coming soon message

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script>
 	import '../app.css';
+
+	function showComingSoon() {
+		alert('Features coming soon...');
+	}
 </script>
 
 <div class="flex h-screen flex-col items-center justify-center gap-2 sm:flex-row sm:gap-4">
 	<input type="text" placeholder="Enter secret..." class="input" />
-	<button class="btn btn-soft btn-primary">Generate link!</button>
+	<button class="btn btn-soft btn-primary" on:click={showComingSoon}>Generate link</button>
 </div>
