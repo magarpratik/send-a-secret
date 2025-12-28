cf5b482363648ee327d487784d1ad43a6419b943
Pratik Magar <magarpratik@live.co.uk>
2025-12-28 20:38:15 +0000

    Fix CSP errors

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -11,7 +11,20 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		csp: {
+			mode: 'auto',
+			directives: {
+				'default-src': ['none'],
+				'script-src': ['self'],
+				'style-src': ['self'],
+				'connect-src': ['self'],
+				'img-src': ['self'],
+				'base-uri': ['none'],
+				'form-action': ['none'],
+				'frame-ancestors': ['none']
+			}
+		}
 	}
 };
 
