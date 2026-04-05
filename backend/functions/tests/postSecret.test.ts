60de8066837603b356ad4873bc9b2d9e823e31a9
Pratik Magar <magarpratik@live.co.uk>
2026-04-05 03:18:40 +0100

    run tests in ci

--- backend/functions/tests/postSecret.test.ts
+++ backend/functions/tests/postSecret.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from "@jest/globals";
 import request from "supertest";
 
 const FUNCTION_URL =
-  "http://localhost:5001/send-a-secret/us-central1/postSecret";
+  "http://localhost:5001/send-a-secret/europe-west1/postSecret";
 
 describe("POST secret", () => {
   test("success", async () => {
