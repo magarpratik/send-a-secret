2dc995bd4f040131e842764520225adf39171dad
Pratik Magar <magarpratik@live.co.uk>
2026-04-06 02:44:08 +0100

    restrict be access

--- backend/functions/src/secrets/getSecret.ts
+++ backend/functions/src/secrets/getSecret.ts
@@ -1,7 +1,7 @@
 import { onCall } from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
 
-export const getSecret = onCall(() => {
+export const getSecret = onCall({ enforceAppCheck: true }, () => {
   logger.info("inside getSecret");
   return { ciphertext: "encrypted-secret" };
 });
--- backend/functions/src/secrets/postSecret.ts
+++ backend/functions/src/secrets/postSecret.ts
@@ -1,7 +1,7 @@
 import { onCall } from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
 
-export const postSecret = onCall(() => {
+export const postSecret = onCall({ enforceAppCheck: true }, () => {
   logger.info("inside postSecret");
   return { secretId: "secret-id-123" };
 });
