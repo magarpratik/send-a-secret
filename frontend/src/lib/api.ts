c49b7df330a119a6677d47b51bc834b6d736fe06
Pratik Magar <magarpratik@live.co.uk>
2026-04-06 14:52:19 +0100

    refactor

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { initializeAppCheck, ReCaptchaEnterpriseProvider } from 'firebase/app-check';
 import { getFunctions, httpsCallable } from 'firebase/functions';
+import { browser } from '$app/environment';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyBJT4AhNLWMoZ1VLgm7dc7Ce-8D0m_tYoA',
@@ -11,7 +12,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-if (typeof window !== 'undefined') {
+if (browser) {
 	initializeAppCheck(app, {
 		provider: new ReCaptchaEnterpriseProvider('6LfF06gsAAAAAK35ipORkrJ2P25OmcDF7ZtQL1hi'),
 		isTokenAutoRefreshEnabled: true
@@ -20,5 +21,8 @@ if (typeof window !== 'undefined') {
 
 const functions = getFunctions(app, 'europe-west1');
 
-export const postSecret = httpsCallable<unknown, { secretId: string }>(functions, 'postSecret');
+export const postSecret = httpsCallable<{ ciphertext: string }, { secretId: string }>(
+	functions,
+	'postSecret'
+);
 export const getSecret = httpsCallable(functions, 'getSecret');
